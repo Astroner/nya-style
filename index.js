999d077d62b4aa1517472ccccbd82243afb41a56
Eugine Astroner <zhenyanikforov@gmail.com>
2019-07-17 01:37:03 +0300

    alphabet update

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 
-module.exports = require('./lib/main.js')
+// module.exports = require('./lib')
+const style = require('./lib');
+console.log(style({
+	body:{
+		"$f/g-1s-hl/x-hm-5o-5": "any"
+	}
+}).parse());
