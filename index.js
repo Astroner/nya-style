a2e8c5db071c1b8636a6d056c37b7478e9e4cd56
Eugine Astroner <zhenyanikforov@gmail.com>
2019-07-17 01:39:05 +0300

    version

--- index.js
+++ index.js
@@ -1,8 +1,2 @@
 
-// module.exports = require('./lib')
-const style = require('./lib');
-console.log(style({
-	body:{
-		"$f/g-1s-hl/x-hm-5o-5": "any"
-	}
-}).parse());
+module.exports = require('./lib')
